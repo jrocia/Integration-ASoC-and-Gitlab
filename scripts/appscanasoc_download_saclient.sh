8d094f6174761d2edc292fa0d6074cd473d1e9c5
jrocia <69405400+jrocia@users.noreply.github.com>
2024-03-01 08:34:15 -0300

    Update appscanasoc_download_saclient.sh

--- scripts/appscanasoc_download_saclient.sh
+++ scripts/appscanasoc_download_saclient.sh
@@ -4,5 +4,7 @@ curl https://cloud.appscan.com/api/SCX/StaticAnalyzer/SAClientUtil?os=linux > $H
 unzip $HOME/SAClientUtil.zip -d $HOME
 rm -f $HOME/SAClientUtil.zip
 mv $HOME/SAClientUtil.* $HOME/SAClientUtil
+export PATH="$HOME/SAClientUtil/bin:${PATH}
 echo 'export PATH="$HOME/SAClientUtil/bin:${PATH}"' >> ~/.bashrc
 source ~/.bashrc
+appscan.sh version
