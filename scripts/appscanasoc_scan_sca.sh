203dc8126a34c53cb996e9ed1cc25ce1d547bbb0
jrocia <69405400+jrocia@users.noreply.github.com>
2024-03-01 13:26:17 -0300

    Update appscanasoc_scan_sca.sh

--- scripts/appscanasoc_scan_sca.sh
+++ scripts/appscanasoc_scan_sca.sh
@@ -1,22 +1,7 @@
-#apt install apt-utils
-#curl -fsSL https://get.docker.com | sh
-
 apt update
 apt upgrade -y
-# Add Docker's official GPG key:
-apt-get update
-apt-get install ca-certificates curl
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt install apt-utils
+curl -fsSL https://get.docker.com | sh
 
 appId=$(cat appId.txt)
 export PATH="$HOME/SAClientUtil/bin:${PATH}"
