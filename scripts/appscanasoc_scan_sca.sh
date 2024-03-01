aaaafe3efef9cb0f8bb274df15b5fdaf0e8b7b0d
jrocia <69405400+jrocia@users.noreply.github.com>
2024-03-01 13:33:22 -0300

    Update appscanasoc_scan_sca.sh

--- scripts/appscanasoc_scan_sca.sh
+++ scripts/appscanasoc_scan_sca.sh
@@ -1,7 +1,7 @@
-apt update
-apt upgrade -y
-apt install apt-utils
-curl -fsSL https://get.docker.com | sh
+#apt update
+#apt upgrade -y
+#apt install apt-utils
+#curl -fsSL https://get.docker.com | sh
 
 appId=$(cat appId.txt)
 export PATH="$HOME/SAClientUtil/bin:${PATH}"
