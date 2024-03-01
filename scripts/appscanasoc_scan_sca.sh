efa273ef2ec62c673cffd3f3125d62467a5a0d20
jrocia <69405400+jrocia@users.noreply.github.com>
2024-03-01 10:37:05 -0300

    Update appscanasoc_scan_sca.sh

--- scripts/appscanasoc_scan_sca.sh
+++ scripts/appscanasoc_scan_sca.sh
@@ -1,8 +1,21 @@
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 appId=$(cat appId.txt)
 export PATH="$HOME/SAClientUtil/bin:${PATH}"
 
-#apt install docker.io
-curl https://get.docker.com/builds/Linux/x86_64/docker-latest.tgz | tar xvz -C /tmp/ && mv /tmp/docker/docker /usr/bin/docker
 appscan.sh prepare_sca -image $image
 
 # Authenticate in ASOC
