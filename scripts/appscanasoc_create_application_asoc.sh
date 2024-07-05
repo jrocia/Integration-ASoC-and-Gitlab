c2d69a743869c8e89111744f3961e72342841b10
jrocia <69405400+jrocia@users.noreply.github.com>
2024-07-05 13:53:13 -0300

    Update appscanasoc_create_application_asoc.sh

--- scripts/appscanasoc_create_application_asoc.sh
+++ scripts/appscanasoc_create_application_asoc.sh
@@ -4,11 +4,11 @@
 #serviceUrl='xxxxxxxxxxxxxxx'
 #assetGroupId='xxxxxxxxxxxxxxx'
 
-asocToken=$(curl -s -X POST --header 'Content-Type:application/json' --header 'Accept:application/json' -d '{"KeyId":"'"${asocApiKeyId}"'","KeySecret":"'"${asocApiKeySecret}"'"}' "https://$serviceUrl/api/v4/Account/ApiKeyLogin" | grep -oP '(?<="Token":\ ")[^"]*')
-appId=$(curl -s -X GET --header 'Authorization: Bearer '"${asocToken}"'' --header 'Accept:application/json' "https://$serviceUrl/api/v4/Apps?%24top=5000&%24filter=Name%20eq%20%27$asocAppName%27&%24select=name%2Cid&%24count=false" | grep -oP '(?<="Id": ")[^"]*')
+asocToken=$(curl -s -X POST --header 'Content-Type:application/json' --header 'Accept:application/json' -d '{"KeyId":"'"$asocApiKeyId"'","KeySecret":"'"$asocApiKeySecret"'"}' "https://$serviceUrl/api/v4/Account/ApiKeyLogin" | grep -oP '(?<="Token":\ ")[^"]*')
+appId=$(curl -s -X GET --header 'Authorization: Bearer '"$asocToken"'' --header 'Accept:application/json' "https://$serviceUrl/api/v4/Apps?%24top=5000&%24filter=Name%20eq%20%27$asocAppName%27&%24select=name%2Cid&%24count=false" | grep -oP '(?<="Id": ")[^"]*')
 if [ -z "$appId" ]; then
-	appId=$(curl -s -X POST --header "Authorization: Bearer ${asocToken}" --header 'Accept:application/json' --header 'Content-Type: application/json' -d '{"Name":"'"$asocAppName"'","AssetGroupId":"'"$assetGroupId"'","UseOnlyAppPresences":false}' "https://$serviceUrl/api/v4/Apps" | grep -oP '(?<="Id": ")[^"]*' | head -n 1);
-	echo "App was created. The appId is $appId";
+	appId=$(curl -s -X POST --header "Authorization: Bearer $asocToken" --header 'Accept:application/json' --header 'Content-Type: application/json' -d '{"Name":"'"$asocAppName"'","AssetGroupId":"'"$assetGroupId"'","UseOnlyAppPresences":false}' "https://$serviceUrl/api/v4/Apps" | grep -oP '(?<="Id": ")[^"]*' | head -n 1);
+	echo "There is no $asocAppName application. It was created. The appId is $appId";
 else 
 	echo "Application name $asocAppName exist. The appId is $appId."
 fi
