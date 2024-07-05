b72a10cd9cb9347eee6a8b84009f0481cce99bae
jrocia <69405400+jrocia@users.noreply.github.com>
2024-07-05 13:58:45 -0300

    Update appscanasoc_create_application_asoc.sh

--- scripts/appscanasoc_create_application_asoc.sh
+++ scripts/appscanasoc_create_application_asoc.sh
@@ -5,6 +5,11 @@
 #assetGroupId='xxxxxxxxxxxxxxx'
 
 asocToken=$(curl -s -X POST --header 'Content-Type:application/json' --header 'Accept:application/json' -d '{"KeyId":"'"$asocApiKeyId"'","KeySecret":"'"$asocApiKeySecret"'"}' "https://$serviceUrl/api/v4/Account/ApiKeyLogin" | grep -oP '(?<="Token":\ ")[^"]*')
+if [ -z "$asocToken" ]; then
+	echo "The token variable is empty";
+    exit 1
+fi
+
 appId=$(curl -s -X GET --header 'Authorization: Bearer '"$asocToken"'' --header 'Accept:application/json' "https://$serviceUrl/api/v4/Apps?%24top=5000&%24filter=Name%20eq%20%27$asocAppName%27&%24select=name%2Cid&%24count=false" | grep -oP '(?<="Id": ")[^"]*')
 if [ -z "$appId" ]; then
 	appId=$(curl -s -X POST --header "Authorization: Bearer $asocToken" --header 'Accept:application/json' --header 'Content-Type: application/json' -d '{"Name":"'"$asocAppName"'","AssetGroupId":"'"$assetGroupId"'","UseOnlyAppPresences":false}' "https://$serviceUrl/api/v4/Apps" | grep -oP '(?<="Id": ")[^"]*' | head -n 1);
