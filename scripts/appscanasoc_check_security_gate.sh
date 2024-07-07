bb5e278c4826f8498e733908dd9378429b62cff5
jrocia <69405400+jrocia@users.noreply.github.com>
2024-07-07 17:01:43 -0300

    Update appscanasoc_check_security_gate.sh

--- scripts/appscanasoc_check_security_gate.sh
+++ scripts/appscanasoc_check_security_gate.sh
@@ -1,31 +1,46 @@
-# Get summary scan and give it to Security Gateway decision
+#asocApiKeyId='xxxxxxxxxxxxx'
+#asocApiKeySecret='xxxxxxxxxxxxx'
+#serviceUrl='xxxxxxxxxxxxx'
+#sevSecGw='xxxxxxxxxxxxx'
+#maxIssuesAllowed=xxxxxxxxxxxxx
+
 scanId=$(cat scanId.txt)
-appscan.sh info -i $scanId > scanStatus.txt
-highIssues=$(cat scanStatus.txt | grep LatestExecution | grep -oP '(?<="NHighIssues":)[^,]*')
-mediumIssues=$(cat scanStatus.txt | grep LatestExecution | grep -oP '(?<="NMediumIssues":)[^,]*')
-lowIssues=$(cat scanStatus.txt | grep LatestExecution | grep -oP '(?<="NLowIssues":)[^,]*')
-totalIssues=$(cat scanStatus.txt | grep LatestExecution | grep -oP '(?<="NIssuesFound":)[^,]*')
-echo "There is $highIssues high issues, $mediumIssues medium issues and $lowIssues low issues."
-if [ "$highIssues" -gt "$maxIssuesAllowed" ] && [ "$sevSecGw" == "highIssues" ]
-  then
+
+asocToken=$(curl -s -X POST --header 'Content-Type:application/json' --header 'Accept:application/json' -d '{"KeyId":"'"$asocApiKeyId"'","KeySecret":"'"$asocApiKeySecret"'"}' "https://$serviceUrl/api/v4/Account/ApiKeyLogin" | grep -oP '(?<="Token":\ ")[^"]*')
+
+if [ -z "$asocToken" ]; then
+	echo "The token variable is empty. Check the authentication process.";
+    exit 1
+fi
+
+curl -s -X GET "https://cloud.appscan.com/api/v4/Scans/Dast/$scanId" -H 'accept:application/json' -H "Authorization:Bearer $asocToken" > scanResult.txt
+criticalIssues=$(cat scanResult.txt | jq -r '.LatestExecution | {NCriticalIssues} | join(" ")')
+highIssues=$(cat scanResult.txt | jq -r '.LatestExecution | {NHighIssues} | join(" ")')
+mediumIssues=$(cat scanResult.txt | jq -r '.LatestExecution | {NMediumIssues} | join(" ")')
+lowIssues=$(cat scanResult.txt | jq -r '.LatestExecution | {NLowIssues} | join(" ")')
+totalIssues=$(cat scanResult.txt | jq -r '.LatestExecution | {NIssuesFound} | join(" ")')
+echo "There is $criticalIssues critical issues, $highIssues high issues, $mediumIssues medium issues and $lowIssues low issues"
+
+if [[ "$criticalIssues" -gt "$maxIssuesAllowed" ]] && [[ "$sevSecGw" == "criticalIssues" ]]; then
     echo "The company policy permit less than $maxIssuesAllowed $sevSecGw severity"
     echo "Security Gate build failed"
     exit 1
-elif [ "$mediumIssues" -gt "$maxIssuesAllowed" ] && [ "$sevSecGw" == "mediumIssues" ]
-  then
+elif [[ "$highIssues" -gt "$maxIssuesAllowed" ]] && [[ "$sevSecGw" == "highIssues" ]]; then
     echo "The company policy permit less than $maxIssuesAllowed $sevSecGw severity"
     echo "Security Gate build failed"
     exit 1
-elif [ "$lowIssues" -gt "$maxIssuesAllowed" ] && [ "$sevSecGw" == "lowIssues" ]
-  then
+elif [[ "$mediumIssues" -gt "$maxIssuesAllowed" ]] && [[ "$sevSecGw" == "mediumIssues" ]]; then
     echo "The company policy permit less than $maxIssuesAllowed $sevSecGw severity"
     echo "Security Gate build failed"
     exit 1
-elif [ "$totalIssues" -gt "$maxIssuesAllowed" ] && [ "$sevSecGw" == "totalIssues" ]
-  then
+elif [[ "$lowIssues" -gt "$maxIssuesAllowed" ]] && [[ "$sevSecGw" == "lowIssues" ]]; then
+    echo "The company policy permit less than $maxIssuesAllowed $sevSecGw severity"
+    echo "Security Gate build failed"
+    exit 1
+elif [[ "$totalIssues" -gt "$maxIssuesAllowed" ]] && [[ "$sevSecGw" == "totalIssues" ]]; then
     echo "The company policy permit less than $maxIssuesAllowed $sevSecGw severity"
     echo "Security Gate build failed"
     exit 1
 fi
 echo "The company policy permit less than $maxIssuesAllowed $sevSecGw severity"
- echo "Security Gate passed"
+echo "Security Gate passed"
