efa4d7f0623f9ed631c8b06f786d33e99e634eff
jrocia <69405400+jrocia@users.noreply.github.com>
2024-07-08 12:28:48 -0300

    Update appscanasoc_get_xml_report.sh

--- scripts/appscanasoc_get_xml_report.sh
+++ scripts/appscanasoc_get_xml_report.sh
@@ -4,7 +4,7 @@
 
 scanId=$(cat scanId.txt)
 
-asocToken=$(curl -s -X POST --header 'Content-Type:application/json' --header 'Accept:application/json' -d '{"KeyId":"'"$asocApiKeyId"'","KeySecret":"'"$asocApiKeySecret"'"}' "https://$serviceUrl/api/v4/Account/ApiKeyLogin" | grep -oP '(?<="Token":\ ")[^"]*')
+asocToken=$(curl -k -s -X POST --header 'Content-Type:application/json' --header 'Accept:application/json' -d '{"KeyId":"'"$asocApiKeyId"'","KeySecret":"'"$asocApiKeySecret"'"}' "https://$serviceUrl/api/v4/Account/ApiKeyLogin" | grep -oP '(?<="Token":\ ")[^"]*')
 
 if [ -z "$asocToken" ]; then
 	echo "The token variable is empty. Check the authentication process.";
@@ -13,12 +13,12 @@ fi
 
 # Request the report
 
-reportId=$(curl -s -X POST --header 'Content-Type:application/json' --header 'Accept:application/json' --header "Authorization:Bearer $asocToken" -d '{"Configuration":{"Summary":true,"Details":true,"Discussion":true,"Overview":true,"TableOfContent":true,"Articles":true,"History":true,"Coverage":true,"MinimizeDetails":true,"ReportFileType":"XML","Title":"","Notes":"","Locale":"en"},"OdataFilter":"","ApplyPolicies":"None"}' "https://$serviceUrl/api/v4/Reports/Security/Scan/$scanId" | grep -oP '(?<="Id":\ ")[^"]*')
+reportId=$(curl -k -s -X POST --header 'Content-Type:application/json' --header 'Accept:application/json' --header "Authorization:Bearer $asocToken" -d '{"Configuration":{"Summary":true,"Details":true,"Discussion":true,"Overview":true,"TableOfContent":true,"Articles":true,"History":true,"Coverage":true,"MinimizeDetails":true,"ReportFileType":"XML","Title":"","Notes":"","Locale":"en"},"OdataFilter":"","ApplyPolicies":"None"}' "https://$serviceUrl/api/v4/Reports/Security/Scan/$scanId" | grep -oP '(?<="Id":\ ")[^"]*')
 
 echo "Report requested. The report id is $reportId."
 
 while true ; do
-    reportStatus=$(curl -s -X GET --header 'Accept:text/xml' --header "Authorization:Bearer $asocToken" "https://$serviceUrl/api/v4/Reports/$reportId/Download" | grep -oP '(?<="Message":\ ")[^"]*')
+    reportStatus=$(curl -k -s -X GET --header 'Accept:text/xml' --header "Authorization:Bearer $asocToken" "https://$serviceUrl/api/v4/Reports/$reportId/Download" | grep -oP '(?<="Message":\ ")[^"]*')
     if [ "$reportStatus" == "Report is not available" ]; then
         echo "Report being generated."
         sleep 30
