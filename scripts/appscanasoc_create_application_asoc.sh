bda31671f974714c772ceed1d0d8bd7638bbd1f6
jrocia <69405400+jrocia@users.noreply.github.com>
2024-07-08 15:41:49 -0300

    Update appscanasoc_create_application_asoc.sh

--- scripts/appscanasoc_create_application_asoc.sh
+++ scripts/appscanasoc_create_application_asoc.sh
@@ -11,7 +11,7 @@ if [ -z "$asocToken" ]; then
     exit 1
 fi
 
-appId=$(curl -s -X GET --header 'Authorization: Bearer '"$asocToken"'' --header 'Accept:application/json' "https://$serviceUrl/api/v4/Apps?%24top=5000&%24filter=Name%20eq%20%27$asocAppName%27&%24select=name%2Cid&%24count=false" | grep -oP '(?<="Id": ")[^"]*')
+appId=$(curl -s -X GET --header 'Authorization: Bearer '"$asocToken"'' --header 'Accept:application/json' "https://$serviceUrl/api/v4/Apps?%24top=5000&%24filter=Name%20eq%20%27$asocAppName%27&%24select=name%2Cid&%24count=false" | grep -oP '(?<="Id":\ ")[^"]*')
 if [ -z "$appId" ]; then
 	appId=$(curl -s -X POST --header "Authorization: Bearer $asocToken" --header 'Accept:application/json' --header 'Content-Type: application/json' -d '{"Name":"'"$asocAppName"'","AssetGroupId":"'"$assetGroupId"'","UseOnlyAppPresences":false}' "https://$serviceUrl/api/v4/Apps" | grep -oP '(?<="Id": ")[^"]*' | head -n 1);
 	echo "There is no $asocAppName application. It was created. The appId is $appId";
