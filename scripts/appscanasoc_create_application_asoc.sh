08d2ad7cacf5385a4620960aaf03983c3ba42113
jrocia <69405400+jrocia@users.noreply.github.com>
2024-07-08 15:21:43 -0300

    Update appscanasoc_create_application_asoc.sh

--- scripts/appscanasoc_create_application_asoc.sh
+++ scripts/appscanasoc_create_application_asoc.sh
@@ -10,9 +10,6 @@ if [ -z "$asocToken" ]; then
 	echo "The token variable is empty. Check the authentication process.";
     exit 1
 fi
-
-appId=$(curl -k -s -X GET --header 'Authorization: Bearer '"$asocToken"'' --header 'Accept:application/json' "https://$serviceUrl/api/v4/Apps?%24top=5000&%24filter=Name%20eq%20%27$asocAppName%27&%24select=name%2Cid&%24count=false" | grep -oP '(?<="Id": ")[^"]*')
-if [ -z "$appId" ]; then
 	appId=$(curl -k -s -X POST --header "Authorization: Bearer $asocToken" --header 'Accept:application/json' --header 'Content-Type: application/json' -d '{"Name":"'"$asocAppName"'","AssetGroupId":"'"$assetGroupId"'","UseOnlyAppPresences":false}' "https://$serviceUrl/api/v4/Apps" | grep -oP '(?<="Id": ")[^"]*' | head -n 1);
 	echo "There is no $asocAppName application. It was created. The appId is $appId";
 else 
