80c8618efccccd25118b7f9041ed8c8cc7538cab
jrocia <69405400+jrocia@users.noreply.github.com>
2024-07-08 11:48:33 -0300

    Update appscanasoc_sast_scan.sh

--- scripts/appscanasoc_sast_scan.sh
+++ scripts/appscanasoc_sast_scan.sh
@@ -7,30 +7,56 @@ appId=$(cat appId.txt)
 echo "Sast" > scanTech.txt
 
 # Downloading and preparing SAClientUtil
-curl https://cloud.appscan.com/api/SCX/StaticAnalyzer/SAClientUtil?os=linux > $HOME/SAClientUtil.zip
-unzip $HOME/SAClientUtil.zip -d $HOME > /dev/null
+curl -k "https://$serviceUrl/api/v4/Tools/SAClientUtil?os=linux" > $HOME/SAClientUtil.zip
+unzip $HOME/SAClientUtil.zip -d $HOME
 rm -f $HOME/SAClientUtil.zip
 mv $HOME/SAClientUtil.* $HOME/SAClientUtil
 export PATH="$HOME/SAClientUtil/bin:${PATH}"
 
 # Generate IRX files based on source root folder downloaded by Gitlab
 appscan.sh prepare
-# Authenticate in ASOC
-appscan.sh api_login -u $asocApiKeyId -P $asocApiKeySecret -persist
 
-# Upload IRX file to ASOC to be analyzed and receive scanId
-appscan.sh queue_analysis -a $appId -n $scanName > output.txt
-scanId=$(sed -n '2p' output.txt)
+# Authenticate in ASOC
+asocToken=$(curl -s -X POST --header 'Content-Type:application/json' --header 'Accept:application/json' -d '{"KeyId":"'"$asocApiKeyId"'","KeySecret":"'"$asocApiKeySecret"'"}' "https://$serviceUrl/api/v4/Account/ApiKeyLogin" | grep -oP '(?<="Token":\ ")[^"]*')
+
+if [ -z "$asocToken" ]; then
+	echo "The token variable is empty. Check the authentication process.";
+    exit 1
+fi
+
+irxFile=$(ls -t *.irx | head -n1)
+
+# Upload IRX file
+if [ -f "$irxFile" ]; then
+    irxFileId=$(curl -s -X 'POST' "https://$serviceUrl/api/v4/FileUpload" -H 'accept:application/json' -H "Authorization:Bearer $asocToken" -H 'Content-Type:multipart/form-data' -F "uploadedFile=@$irxFile" | grep -oP '(?<="FileId":\ ")[^"]*');
+    echo "$irxFile exist. It will be uploaded to ASoC. IRX file id is $irxFileId.";
+else
+    echo "IRX file not identified.";
+fi
+
+# Start scan
+
+scanId=$(curl -s -X 'POST' "https://$serviceUrl/api/v4/Scans/Sast" -H 'accept:application/json' -H "Authorization:Bearer $asocToken" -H 'Content-Type:application/json' -d '{"AppId":"'"$appId"'","ApplicationFileId":"'"$irxFileId"'","ClientType":"user-site","EnableMailNotification":false,"Execute":true,"Locale":"en","Personal":false,"ScanName":"'"SAST $scanName $irxFile"'","EnablementMessage":"","FullyAutomatic":true}' | jq -r '. | {Id} | join(" ")');
+echo "Scan started, scanId $scanId";
+
 echo "The scan name is $scanName and scanId is $scanId"
 echo $scanId > scanId.txt
 
-# Check Scan Status
-resultScan=$(appscan.sh status -i $scanId)
+# Check status scan and keep it in loop until Ready status.
+scanStatus=$(curl -s -X 'GET' "https://$serviceUrl/api/v4/Scans/Sast/$scanId" -H 'accept:application/json' -H "Authorization:Bearer $asocToken" | jq -r '.LatestExecution | {Status} | join(" ")');
+echo $scanStatus
+
 while true ; do 
-    resultScan=$(appscan.sh status -i $scanId)
-    echo $resultScan
-        if [ "$resultScan" != "Running" ]; then 
-            break
-        fi
+    scanStatus=$(curl -s -X 'GET' "https://$serviceUrl/api/v4/Scans/Sast/$scanId" -H 'accept:application/json' -H "Authorization:Bearer $asocToken" | jq -r '.LatestExecution | {Status} | join(" ")');
+    if [ "$scanStatus" == "Running" ] || [ "$scanStatus" == "InQueue" ]; then
+        echo $scanStatus
+    elif [ "$scanStatus" == "Failed" ]; then
+        echo $scanStatus
+        echo "Scan Failed. Check ASOC logs"
+        exit 1
+    else
+        echo $scanStatus
+        break
+    fi
     sleep 60
 done
