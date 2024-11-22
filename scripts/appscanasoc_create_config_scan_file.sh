ac5cc59005d9949e5832daad55a338e131c6351d
jrocia <69405400+jrocia@users.noreply.github.com>
2024-11-22 10:06:06 -0300

    Update appscanasoc_create_config_scan_file.sh

--- scripts/appscanasoc_create_config_scan_file.sh
+++ scripts/appscanasoc_create_config_scan_file.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><Configuration enableSecrets=\"true\" sourceCodeOnly=\"true\" staticAnalysisOnly=\"true\"><Targets><Target path=\"$PWD\"></Target></Targets></Configuration>" > appscan-config.xml
+echo "Only these files will be scanned:"
+echo $(git diff --name-only HEAD HEAD~1)
 diffFilesList=$(git diff --name-only HEAD HEAD~1)
 readarray -t diffFiles <<< "$diffFilesList"
 for i in ${diffFiles[@]}; do sed -i "s|\(</Target>\)|<Include>$i</Include>\1|" appscan-config.xml; done
