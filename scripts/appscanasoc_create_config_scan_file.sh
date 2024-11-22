2341ebdba4b511064aaebe3ec81b7a5aa9142f2b
jrocia <69405400+jrocia@users.noreply.github.com>
2024-11-22 13:59:48 -0300

    Update appscanasoc_create_config_scan_file.sh

--- scripts/appscanasoc_create_config_scan_file.sh
+++ scripts/appscanasoc_create_config_scan_file.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><Configuration enableSecrets=\"true\" sourceCodeOnly=\"true\" staticAnalysisOnly=\"true\"><Targets><Target path=\"$PWD\"></Target></Targets></Configuration>" > appscan-config.xml
-echo "Only these files will be scanned:"
-echo $(git diff --name-only HEAD HEAD~1)
-diffFilesList=$(git diff --name-only HEAD HEAD~1)
-readarray -t diffFiles <<< "$diffFilesList"
-for i in ${diffFiles[@]}; do sed -i "s|\(</Target>\)|<Include>$i</Include>\1|" appscan-config.xml; done
+
+if [ "$scanLatestCommitFiles" = 'yes' ]; then
+  echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><Configuration enableSecrets=\"true\" sourceCodeOnly=\"true\" staticAnalysisOnly=\"true\"><Targets><Target path=\"$PWD\"></Target></Targets></Configuration>" > appscan-config.xml
+  echo "Only these files will be scanned:"
+  echo $(git diff --name-only HEAD HEAD~1)
+  diffFilesList=$(git diff --name-only HEAD HEAD~1)
+  readarray -t diffFiles <<< "$diffFilesList"
+  for i in ${diffFiles[@]}; do sed -i "s|\(</Target>\)|<Include>$i</Include>\1|" appscan-config.xml; done
+else
+  echo "All files in the repository will be scanned."
+fi
