13ccabaa56dbfcb0d43afdb9741ba5202f139626
jrocia <69405400+jrocia@users.noreply.github.com>
2024-11-22 14:09:56 -0300

    Update appscanasoc_create_config_scan_file.sh

--- scripts/appscanasoc_create_config_scan_file.sh
+++ scripts/appscanasoc_create_config_scan_file.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ -z "$scanLatestCommitFiles" || ( "$scanLatestCommitFiles" != "yes" && "$scanLatestCommitFiles" != "no" ) ]]; then
+  echo "The variable scanLatestCommitFiles must be 'yes' or 'no', and it cannot be empty."
+  exit 1
+fi
+
 if [ "$scanLatestCommitFiles" = 'yes' ]; then
   echo "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><Configuration enableSecrets=\"true\" sourceCodeOnly=\"true\" staticAnalysisOnly=\"true\"><Targets><Target path=\"$PWD\"></Target></Targets></Configuration>" > appscan-config.xml
   echo "Only these files will be scanned:"
